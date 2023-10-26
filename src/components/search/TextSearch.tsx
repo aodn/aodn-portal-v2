44aac0629d8163b77cde33a00d5fea3a0e30b26f
Raymond Ng <ng.raymond.au@gmail.com>
2023-10-26 21:18:48 +1100

    add collapse animation

--- src/components/search/TextSearch.tsx
+++ src/components/search/TextSearch.tsx
@@ -8,6 +8,7 @@ import {margin} from '../common/constants';
 import NoBorderButton from '../common/buttons/NoBorderButton';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import RemovableFilter from '../common/filters/RemovableFilter';
+import Collapse from '@mui/material/Collapse';
 import React from 'react';
 
 const StyledTextField = styled(TextField)<TextFieldProps>(({ theme }) => ({
@@ -99,28 +100,29 @@ const TextSearch = () => {
                     </Grid>
                     <Grid item>{searchButton()}</Grid>
                 </Grid>
-                <Grid 
-                    container 
-                    gap={2}
-                    justifyContent={'center'} 
-                    sx= {{display: showFilters ? 'flex' : 'none'}}
-                >
-                    <Grid item xs={8} sx={{textAlign:'center'}}>
-                        <NoBorderButton
-                            endIcon={<ArrowDropUpIcon/>}
-                            sx={{
-                                fontWeight: 'bold',
-                            }}
-                            onClick={() => {setShowFilters(false)}}
-                        >
-                            Search Filters 
-                        </NoBorderButton>
+                <Collapse orientation="vertical" in={showFilters}>
+                    <Grid 
+                        container 
+                        gap={2}
+                        justifyContent={'center'}    
+                    >
+                        <Grid item xs={8} sx={{textAlign:'center'}}>
+                            <NoBorderButton
+                                endIcon={<ArrowDropUpIcon/>}
+                                sx={{
+                                    fontWeight: 'bold',
+                                }}
+                                onClick={() => {setShowFilters(false)}}
+                            >
+                                Search Filters 
+                            </NoBorderButton>
+                        </Grid>
+                        <Grid item xs={8}><RemovableFilter title='Parameter' url='/filters/tune.png'/></Grid>
+                        <Grid item xs={8}><RemovableFilter title='Platform' url='/filters/platform.png'/></Grid>
+                        <Grid item xs={8}><RemovableFilter title='Organization' url='/filters/organization.png'/></Grid>
+                        <Grid item xs={8}><RemovableFilter title='Data' url='/filters/data.png'/></Grid>
                     </Grid>
-                    <Grid item xs={8}><RemovableFilter title='Parameter' url='/filters/tune.png'/></Grid>
-                    <Grid item xs={8}><RemovableFilter title='Platform' url='/filters/platform.png'/></Grid>
-                    <Grid item xs={8}><RemovableFilter title='Organization' url='/filters/organization.png'/></Grid>
-                    <Grid item xs={8}><RemovableFilter title='Data' url='/filters/data.png'/></Grid>
-                </Grid>
+                </Collapse>
             </Grid>
         </Grid>
     );
