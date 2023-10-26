993abc91c72489a64dbfc2949057842644a9b180
Raymond Ng <ng.raymond.au@gmail.com>
2023-10-26 21:46:20 +1100

    add filter footer

--- src/components/search/TextSearch.tsx
+++ src/components/search/TextSearch.tsx
@@ -9,6 +9,10 @@ import NoBorderButton from '../common/buttons/NoBorderButton';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import RemovableFilter from '../common/filters/RemovableFilter';
 import Collapse from '@mui/material/Collapse';
+import SettingsOverscanIcon from '@mui/icons-material/SettingsOverscan';
+import AddIcon from '@mui/icons-material/Add';
+import CheckIcon from '@mui/icons-material/Check';
+import Divider from '@mui/material/Divider';
 import React from 'react';
 
 const StyledTextField = styled(TextField)<TextFieldProps>(({ theme }) => ({
@@ -58,6 +62,46 @@ const filterButton = (setValue: React.Dispatch<React.SetStateAction<boolean>>) =
             Filters
         </Button>
     </InputAdornment>
+
+const filterFooter = () => 
+    <Grid item xs={8}>
+        <Grid 
+            container
+            columns={6}
+        > 
+            <Grid item xs={1}>
+                <NoBorderButton 
+                    endIcon={<AddIcon/>}
+                    sx={{
+                        color: grey['searchButtonText'],
+                    }}
+                >
+                    Add Filters
+                </NoBorderButton>
+            </Grid>
+            <Grid item xs={4} sx={{textAlign: 'center'}}>
+                <NoBorderButton
+                    endIcon={<SettingsOverscanIcon/>}
+                    sx={{
+                        color: grey['searchButtonText'],
+                    }}
+                >
+                    Expand All Filters
+                </NoBorderButton>
+            </Grid>
+            <Grid item xs={1} sx={{textAlign: 'right'}}>
+            <NoBorderButton 
+                    endIcon={<CheckIcon/>}
+                    sx={{
+                        color: grey['searchButtonText'],
+                    }}
+                >
+                    Apply Filters
+                </NoBorderButton>                                    
+            </Grid>
+        </Grid>
+        <Divider></Divider>
+    </Grid>;
 
 const searchButton = () =>
     <Button
@@ -121,6 +165,11 @@ const TextSearch = () => {
                         <Grid item xs={8}><RemovableFilter title='Platform' url='/filters/platform.png'/></Grid>
                         <Grid item xs={8}><RemovableFilter title='Organization' url='/filters/organization.png'/></Grid>
                         <Grid item xs={8}><RemovableFilter title='Data' url='/filters/data.png'/></Grid>
+                        {
+                            // The bottom section of filter group, contains three button
+                            filterFooter()
+                        }
+
                     </Grid>
                 </Collapse>
             </Grid>
