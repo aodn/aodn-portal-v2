f483c0bb7136a15653884ff963cb7e341f35f959
utas-raymondng <manfai.ng@utas.edu.au>
2024-03-19 12:53:23 +1100

    Fix lint

--- src/components/common/datetime/DatePicker.tsx
+++ src/components/common/datetime/DatePicker.tsx
@@ -1,14 +1,14 @@
-import * as React from 'react';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import * as React from "react";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import {DateTimePicker, DateTimePickerProps} from '@mui/x-date-pickers/DateTimePicker';
 // TODO: Temp works but need to check user locale on date time format
-import 'dayjs/locale/en-gb';
+import "dayjs/locale/en-gb";
 
 const DatePicker = (props: DateTimePickerProps<any>) => {
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={'en-gb'}>
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={"en-gb"}>
             <DateTimePicker {...props} />
         </LocalizationProvider>
     );
