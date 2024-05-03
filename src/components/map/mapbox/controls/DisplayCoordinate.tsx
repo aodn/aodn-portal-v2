bff385528866e9a107cf2ff5e6679cd71d97fbef
utas-raymondng <manfai.ng@utas.edu.au>
2024-05-03 15:29:09 +1000

    Fix missing import

--- src/components/map/mapbox/controls/DisplayCoordinate.tsx
+++ src/components/map/mapbox/controls/DisplayCoordinate.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import MapContext from "../MapContext";
 import { zIndex } from "../../../common/constants";
 
