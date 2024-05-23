70a4c0e90dd916bdaa65e619f61da4189885e270
utas-raymondng <manfai.ng@utas.edu.au>
2024-05-23 14:25:34 +1000

    fix error on yarn

--- src/components/common/store/__test__/searchReducer.test.tsx
+++ src/components/common/store/__test__/searchReducer.test.tsx
@@ -4,10 +4,29 @@ import default_thumbnail from "@/assets/images/default-thumbnail.png";
 
 describe("Search Reducer Function Test", () => {
   it("Empty links return default thumbnail", () => {
-    const item =
-      "{ 'id': 'ba9110f1-072c-4d15-8328-2091be983991', 'index': '1', 'itemType': 'Collection', 'links': [], 'properties': {}}";
+    const item = {
+      id: "ba9110f1-072c-4d15-8328-2091be983991",
+      index: "1",
+      itemType: "Collection",
+      links: [],
+      properties: {},
+    };
 
     const collection: OGCCollection = Object.assign(new OGCCollection(), item);
     expect(collection.findThumbnail()).toEqual(default_thumbnail);
+  });
+
+  it("Verify OGCCollection status is correct", () => {
+    const item = {
+      id: "ba9110f1-072c-4d15-8328-2091be983991",
+      index: "1",
+      itemType: "Collection",
+      links: [],
+      properties: { STATUS: "completed" },
+    };
+
+    const collection: OGCCollection = Object.assign(new OGCCollection(), item);
+    console.debug(collection);
+    expect(collection.getStatus()).toEqual("completed");
   });
 });
--- src/components/common/store/searchReducer.tsx
+++ src/components/common/store/searchReducer.tsx
@@ -27,6 +27,8 @@ interface Spatial {
 }
 
 export class OGCCollection {
+
+  private propValue: Map<string, any>;
   id: string;
   // This index is used to show the ordering 1, 2, 3...
   index?: string;
