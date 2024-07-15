b0e6583dc377c82c22d7b8a8c6ed05e861ed317c
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-07-15 13:46:08 +1000

    :art: generalise text block usage

---
+++ src/pages/detail-page/subpages/components/PlainTextBlock.tsx
@@ -0,0 +1,34 @@
+import BlockList from "./BlockList";
+import React, { ReactNode, useMemo } from "react";
+import PlainTextFragment from "./PlainTextFragmentProps";
+
+interface PlainTextBlockProps {
+  title: string;
+  texts: string[];
+}
+const PlainTextBlock: React.FC<PlainTextBlockProps> = ({ title, texts }) => {
+  const textList: ReactNode[] = useMemo(() => {
+    const returnedList: ReactNode[] = [];
+    texts?.map((text) => {
+      const showedText = [];
+      if (text.includes("\n")) {
+        const splitText = text.split("\n");
+        splitText.forEach((textPart, index) => {
+          if (index > 0) {
+            showedText.push(<br />);
+          }
+          showedText.push(textPart);
+        });
+      } else {
+        showedText.push(text);
+      }
+
+      returnedList.push(
+        <PlainTextFragment key={text}>{showedText}</PlainTextFragment>
+      );
+    });
+    return returnedList;
+  }, [texts]);
+  return <BlockList title={title} childrenList={textList} />;
+};
+export default PlainTextBlock;
