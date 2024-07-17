621c8308e74307202349e1338fbfc31921fb1977
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-07-17 13:40:58 +1000

    :art: add citation and constraints

--- src/pages/detail-page/subpages/tab-panels/CitationPanel.tsx
+++ src/pages/detail-page/subpages/tab-panels/CitationPanel.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useMemo, useState } from "react";
 import { useDetailPageContext } from "../../context/detail-page-context";
 import ContactBlock from "../components/ContactBlock";
 import NavigatablePanel from "../components/NavigatablePanel";
+import PlainTextBlock from "../components/PlainTextBlock";
+import PlainCollapseBlock from "../components/PlainCollapseBlock";
 
 const CitationPanel = () => {
   const context = useDetailPageContext();
@@ -12,6 +14,38 @@ const CitationPanel = () => {
         ?.filter((contact) => contact.roles.includes("citation")),
     [context.collection]
   );
+  const suggestedCitation = useMemo(
+    () => context.collection?.getCitation()?.suggestedCitation,
+    [context.collection]
+  );
+  const otherConstraints = useMemo(
+    () => context.collection?.getCitation()?.otherConstraints,
+    [context.collection]
+  );
+  const useLimitations = useMemo(
+    () => context.collection?.getCitation()?.useLimitations,
+    [context.collection]
+  );
+
+  const constraints = useMemo(() => {
+    const constraintItems: { title: string; content: string[] }[] = [];
+    useLimitations?.forEach((limitation) => {
+      constraintItems.push({
+        title: "Use Limitation",
+        content: [limitation],
+      });
+    });
+    otherConstraints?.forEach((constraint) => {
+      if (!constraint) {
+        return;
+      }
+      constraintItems.push({
+        title: "Other Constraint",
+        content: [constraint],
+      });
+    });
+    return constraintItems;
+  }, [otherConstraints, useLimitations]);
 
   const [isLoading, setIsLoading] = useState(true);
 
@@ -40,14 +74,21 @@ const CitationPanel = () => {
       },
       {
         title: "Suggested Citation",
-        component: <div>Suggested Citation</div>,
+        component: (
+          <PlainTextBlock
+            title="Suggested Citation"
+            texts={suggestedCitation ? [suggestedCitation] : []}
+          />
+        ),
       },
       {
         title: "Constraints",
-        component: <div>Constraints</div>,
+        component: (
+          <PlainCollapseBlock title="Constraints" items={constraints} />
+        ),
       },
     ],
-    [citationContacts]
+    [citationContacts, constraints, suggestedCitation]
   );
 
   return <NavigatablePanel childrenList={blocks} isLoading={isLoading} />;
