b1c97031444061b8108e7c4272d310189574d94e
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-08-02 14:51:21 +1000

    add associated records

---
+++ src/components/list/AssociatedRecordList.tsx
@@ -0,0 +1,44 @@
+import { IAssociatedRecord } from "../common/store/OGCCollectionDefinitions";
+import React, { ReactNode, useCallback, useMemo } from "react";
+import CollapseItem from "./listItem/CollapseItem";
+import PlainTextItem from "./listItem/PlainTextItem";
+import { ButtonBase } from "@mui/material";
+import { pageDefault } from "../common/constants";
+import ExpandableList from "./ExpandableList";
+
+interface AssociatedRecordListProps {
+  title: string;
+  records: IAssociatedRecord[];
+}
+
+const AssociatedRecordList: React.FC<AssociatedRecordListProps> = ({
+  title,
+  records,
+}) => {
+  const openRecord = useCallback((uuid: string) => {
+    const searchParams = new URLSearchParams();
+    searchParams.append("uuid", uuid);
+    const url = pageDefault.details + "?" + searchParams.toString();
+    window.open(url, "_blank");
+  }, []);
+
+  const collapseComponents: ReactNode[] = useMemo(() => {
+    const components: ReactNode[] = [];
+    records?.map((record, index) => {
+      components.push(
+        <CollapseItem title={record.title} key={index}>
+          <PlainTextItem key={index}>
+            <ButtonBase onClick={() => openRecord(record.uuid)}>
+              {record.abstract}
+            </ButtonBase>
+          </PlainTextItem>
+        </CollapseItem>
+      );
+    });
+    return components;
+  }, [openRecord, records]);
+
+  return <ExpandableList title={title} childrenList={collapseComponents} />;
+};
+
+export default AssociatedRecordList;
