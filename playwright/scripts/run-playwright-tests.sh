cfc5601e7d8b92416fa847fe1875bf02302d2601
AR-RIFAT <ar.rifat.954@gmail.com>
2024-08-07 09:15:00 +1000

    :construction_worker: updated docker compose command

--- playwright/scripts/run-playwright-tests.sh
+++ playwright/scripts/run-playwright-tests.sh
@@ -9,17 +9,17 @@ test_needs_build=$([[ $PYPROJECT_TOML_MODIFIED -gt $LAST_BUILD_TIME ]] && echo "
 
 if [[ $web_needs_build == "true" && $test_needs_build == "true" ]]; then
   echo "Running docker build..."
-  docker-compose -f playwright/docker-compose.yml build
+  docker compose -f playwright/docker-compose.yml build
 elif [[ $web_needs_build == "true" ]]; then
   echo "Rebuilding aodn-portal-v2..."
-  docker-compose -f playwright/docker-compose.yml build web
+  docker compose -f playwright/docker-compose.yml build web
 elif [[ $test_needs_build == "true" ]]; then
   echo "Rebuilding playwright test suite..."
-  docker-compose -f playwright/docker-compose.yml build tests
+  docker compose -f playwright/docker-compose.yml build tests
 else
   echo "Running docker-compose"
 fi
 
 touch playwright/.docker_build_time
 
-docker-compose -f playwright/docker-compose.yml up --abort-on-container-exit --exit-code-from tests
+docker compose -f playwright/docker-compose.yml up --abort-on-container-exit --exit-code-from tests
