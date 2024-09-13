743af64d85a417b4fb90b6943565309910a321f5
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-09-13 16:34:21 +1000

    new icons

---
+++ src/components/icon/PlusIcon.tsx
@@ -0,0 +1,17 @@
+import { SvgIcon } from "@mui/material";
+
+const PlusIcon = () => {
+  return (
+    <SvgIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
+      <path
+        d="M11.5 4.35156V17.8516M18.25 11.1016H4.75"
+        stroke="#54BCEB"
+        strokeWidth="1.35"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </SvgIcon>
+  );
+};
+
+export default PlusIcon;
