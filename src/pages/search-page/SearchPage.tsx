4f0cacfc85b7d5be68fc8e9467dc49826f9de4fb
utas-raymondng <manfai.ng@utas.edu.au>
2024-09-16 14:54:57 +1000

    Add sort by rank to the existing sort

--- src/pages/search-page/SearchPage.tsx
+++ src/pages/search-page/SearchPage.tsx
@@ -259,11 +259,21 @@ const SearchPage = () => {
     (v: SortResultEnum) => {
       switch (v) {
         case SortResultEnum.RELEVANT:
-          dispatch(updateSortBy([{ field: "score", order: "DESC" }]));
+          dispatch(
+            updateSortBy([
+              { field: "score", order: "DESC" },
+              { field: "rank", order: "DESC" },
+            ])
+          );
           break;
 
         case SortResultEnum.TITLE:
-          dispatch(updateSortBy([{ field: "title", order: "ASC" }]));
+          dispatch(
+            updateSortBy([
+              { field: "title", order: "ASC" },
+              { field: "rank", order: "DESC" },
+            ])
+          );
           break;
 
         case SortResultEnum.POPULARITY:
@@ -271,7 +281,12 @@ const SearchPage = () => {
           break;
 
         case SortResultEnum.MODIFIED:
-          dispatch(updateSortBy([{ field: "temporal", order: "DESC" }]));
+          dispatch(
+            updateSortBy([
+              { field: "temporal", order: "DESC" },
+              { field: "rank", order: "DESC" },
+            ])
+          );
           break;
       }
 
