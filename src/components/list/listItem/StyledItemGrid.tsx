544f88f3bab08b2db6b504a5ae283511520b6ca1
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-09-19 14:10:49 +1000

    change root item grid to provide on hover context

--- src/components/list/listItem/StyledItemGrid.tsx
+++ src/components/list/listItem/StyledItemGrid.tsx
@@ -1,12 +1,40 @@
 import { styled } from "@mui/material/styles";
-import { Grid } from "@mui/material";
+import { Grid, GridProps } from "@mui/material";
+import React, { createContext, useContext, useState } from "react";
 
-const StyledItemGrid = styled(Grid)(({ theme }) => ({
+const StyledGrid = styled(Grid)(({ theme }) => ({
   backgroundColor: theme.palette.detail.listItemBG,
   margin: theme.mp.sm,
   borderRadius: theme.borderRadius.sm,
   width: "95%",
   padding: `${theme.mp.sm} ${theme.mp.xlg}`,
 }));
+
+interface HoverContextProps {
+  isOnHover: boolean;
+}
+
+const HoverContext = createContext<HoverContextProps | undefined>(undefined);
+export const useHoverContext = () => {
+  const context = useContext(HoverContext);
+  if (!context) {
+    throw new Error("useHoverContext must be used within a HoverProvider");
+  }
+  return context;
+};
+const StyledItemGrid: React.FC<GridProps> = (props) => {
+  const [isOnHover, setisOnHover] = useState<boolean>(false);
+  return (
+    <HoverContext.Provider value={{ isOnHover }}>
+      <StyledGrid
+        {...props}
+        onMouseEnter={() => setisOnHover(true)}
+        onMouseLeave={() => setisOnHover(false)}
+      >
+        {props.children}
+      </StyledGrid>
+    </HoverContext.Provider>
+  );
+};
 
 export default StyledItemGrid;
