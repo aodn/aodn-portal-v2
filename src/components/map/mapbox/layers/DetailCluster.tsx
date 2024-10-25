ba44ff87771b61984b4f23c340a3137853de6040
HuaizhiDai <huaizhi.dai@utas.edu.au>
2024-10-25 17:12:56 +1100

    a new detail cluster layer only for detail page

---
+++ src/components/map/mapbox/layers/DetailCluster.tsx
@@ -0,0 +1,266 @@
+import { FC, useCallback, useContext, useEffect, useMemo } from "react";
+import MapContext from "../MapContext";
+import { GeoJSONSource } from "mapbox-gl";
+import {
+  defaultMouseEnterEventHandler,
+  defaultMouseLeaveEventHandler,
+  findSuitableVisiblePoint,
+  LayersProps,
+} from "./Layers";
+import { mergeWithDefaults } from "../../../common/utils";
+import SpatialExtents from "../component/SpatialExtents";
+import SpiderDiagram from "../component/SpiderDiagram";
+import { Feature, Point } from "geojson";
+import { MapDefaultConfig } from "../constants";
+import { generateFeatureCollectiionFrom } from "../../../../utils/GeoJsonUtils";
+
+interface DetailClusterSize {
+  default?: number | string;
+  medium: number | string;
+  large: number | string;
+  extra_large: number | string;
+}
+
+interface DetailClusterConfig {
+  pointCountThresholds: DetailClusterSize;
+  clusterMaxZoom: number;
+  clusterRadius: number;
+  clusterCircleSize: DetailClusterSize;
+  clusterCircleColor: DetailClusterSize;
+  clusterCircleOpacity: number;
+  clusterCircleStrokeWidth: number;
+  clusterCircleStrokeColor: string;
+  clusterCircleTextSize: number;
+  unclusterPointColor: string;
+  unclusterPointOpacity: number;
+  unclusterPointStrokeWidth: number;
+  unclusterPointStrokeColor: string;
+  unclusterPointRadius: number;
+}
+
+interface DetailClusterProps extends LayersProps {
+  // Some method inherit from LayersProps
+  clusterLayerConfig?: Partial<DetailClusterConfig>;
+}
+
+const defaultDetailClusterConfig: DetailClusterConfig = {
+  // point count thresholds define the boundaries between different cluster sizes.
+  pointCountThresholds: {
+    medium: 20,
+    large: 30,
+    extra_large: 50,
+  },
+  clusterMaxZoom: MapDefaultConfig.MAX_ZOOM,
+  clusterRadius: 50,
+  // circle sizes define the radius(px) of the circles used to represent clusters on the map.
+  clusterCircleSize: {
+    default: 20,
+    medium: 30,
+    large: 40,
+    extra_large: 60,
+  },
+  //cluster circle colors define the colors used for the circles representing clusters of different sizes.
+  clusterCircleColor: {
+    default: "#51bbd6",
+    medium: "#f1f075",
+    large: "#f28cb1",
+    extra_large: "#fe8cf1",
+  },
+  clusterCircleOpacity: 0.6,
+  clusterCircleStrokeWidth: 1,
+  clusterCircleStrokeColor: "#fff",
+  clusterCircleTextSize: 12,
+  unclusterPointColor: "#51bbd6",
+  unclusterPointOpacity: 1,
+  unclusterPointStrokeWidth: 1,
+  unclusterPointStrokeColor: "#fff",
+  unclusterPointRadius: 8,
+};
+
+// These function help to get the correct id and reduce the need to set those id in the
+// useEffect list
+export const getLayerId = (id: string | undefined) => `cluster-layer-${id}`;
+
+export const getClusterSourceId = (layerId: string) => `${layerId}-source`;
+
+export const getClusterLayerId = (layerId: string) => `${layerId}-clusters`;
+
+export const getUnclusterPointId = (layerId: string) =>
+  `${layerId}-unclustered-point`;
+
+const DetailCluster: FC<DetailClusterProps> = ({
+  features = generateFeatureCollectiionFrom(undefined),
+  selectedUuids,
+  onDatasetSelected,
+  clusterLayerConfig,
+  showFullMap,
+}) => {
+  const { map } = useContext(MapContext);
+
+  const layerId = useMemo(() => getLayerId(map?.getContainer().id), [map]);
+
+  const clusterSourceId = useMemo(() => getClusterSourceId(layerId), [layerId]);
+
+  const clusterLayer = useMemo(() => getClusterLayerId(layerId), [layerId]);
+
+  const unclusterPointLayer = useMemo(
+    () => getUnclusterPointId(layerId),
+    [layerId]
+  );
+
+  // This is used to render the cluster circle and add event handle to circles
+  useEffect(() => {
+    if (map === null) return;
+
+    const createLayers = () => {
+      if (map?.getSource(clusterSourceId)) return;
+
+      const config = mergeWithDefaults(
+        defaultDetailClusterConfig,
+        clusterLayerConfig
+      );
+
+      map?.setMaxZoom(config.clusterMaxZoom);
+
+      map?.addSource(clusterSourceId, {
+        type: "geojson",
+        data: findSuitableVisiblePoint(
+          {
+            type: "FeatureCollection",
+            features: new Array<Feature<Point>>(),
+          },
+          map
+        ),
+        cluster: true,
+        clusterMaxZoom: config.clusterMaxZoom,
+        clusterRadius: 50,
+        clusterProperties: {
+          count: ["+", ["get", "count"]],
+        },
+      });
+
+      map?.addLayer({
+        id: clusterLayer,
+        type: "circle",
+        source: clusterSourceId,
+        filter: ["has", "point_count"],
+        paint: {
+          "circle-stroke-width": config.clusterCircleStrokeWidth,
+          "circle-stroke-color": config.clusterCircleStrokeColor,
+          "circle-opacity": config.clusterCircleOpacity,
+          "circle-color": [
+            "step",
+            ["get", "count"],
+            config.clusterCircleColor.default,
+            config.pointCountThresholds.medium,
+            config.clusterCircleColor.medium,
+            config.pointCountThresholds.large,
+            config.clusterCircleColor.large,
+            config.pointCountThresholds.extra_large,
+            config.clusterCircleColor.extra_large,
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "count"],
+            config.clusterCircleSize.default,
+            config.pointCountThresholds.medium,
+            config.clusterCircleSize.medium,
+            config.pointCountThresholds.large,
+            config.clusterCircleSize.large,
+            config.pointCountThresholds.extra_large,
+            config.clusterCircleSize.extra_large,
+          ],
+        },
+      });
+
+      map?.addLayer({
+        id: `${layerId}-cluster-count`,
+        type: "symbol",
+        source: clusterSourceId,
+        filter: ["has", "point_count"],
+        layout: {
+          "text-field": "{count}",
+          "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
+          "text-size": config.clusterCircleTextSize,
+        },
+      });
+
+      map?.addLayer({
+        id: unclusterPointLayer,
+        type: "circle",
+        source: clusterSourceId,
+        filter: ["!", ["has", "point_count"]],
+        paint: {
+          "circle-opacity": config.unclusterPointOpacity,
+          "circle-color": config.unclusterPointColor,
+          "circle-radius": config.unclusterPointRadius,
+          "circle-stroke-width": config.unclusterPointStrokeWidth,
+          "circle-stroke-color": config.unclusterPointStrokeColor,
+        },
+      });
+
+      map?.on("mouseenter", clusterLayer, defaultMouseEnterEventHandler);
+      map?.on("mouseleave", clusterLayer, defaultMouseLeaveEventHandler);
+    };
+
+    map?.once("load", createLayers);
+    map?.on("styledata", createLayers);
+
+    return () => {
+      map?.off("mouseenter", clusterLayer, defaultMouseEnterEventHandler);
+      map?.off("mouseleave", clusterLayer, defaultMouseLeaveEventHandler);
+
+      try {
+        if (map?.getLayer(clusterLayer)) map?.removeLayer(clusterLayer);
+        if (map?.getLayer(`${layerId}-cluster-count`))
+          map?.removeLayer(`${layerId}-cluster-count`);
+        if (map?.getLayer(unclusterPointLayer))
+          map?.removeLayer(unclusterPointLayer);
+        if (map?.getSource(clusterSourceId)) map?.removeSource(clusterSourceId);
+      } catch (error) {
+        // Handle error
+      }
+    };
+  }, [
+    clusterLayer,
+    clusterLayerConfig,
+    clusterSourceId,
+    layerId,
+    map,
+    unclusterPointLayer,
+  ]);
+
+  const updateSource = useCallback(() => {
+    if (map?.getSource(clusterSourceId)) {
+      (map?.getSource(clusterSourceId) as GeoJSONSource).setData(features);
+    }
+  }, [map, clusterSourceId, features]);
+
+  useEffect(() => {
+    updateSource();
+    map?.on("styledata", updateSource);
+    return () => {
+      map?.off("styledata", updateSource);
+    };
+  }, [map, updateSource]);
+
+  return (
+    <>
+      <SpatialExtents
+        layerId={unclusterPointLayer}
+        selectedUuids={selectedUuids}
+        addedLayerIds={[clusterLayer, unclusterPointLayer]}
+        onDatasetSelected={onDatasetSelected}
+      />
+      <SpiderDiagram
+        clusterLayer={clusterLayer}
+        clusterSourceId={clusterSourceId}
+        unclusterPointLayer={unclusterPointLayer}
+        onDatasetSelected={onDatasetSelected}
+        showFullMap={showFullMap}
+      />
+    </>
+  );
+};
+
+export default DetailCluster;
