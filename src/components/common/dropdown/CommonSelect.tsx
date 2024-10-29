92be2b4640d4e0eae811ec8cb3f79b0267aba5a6
utas-raymondng <manfai.ng@utas.edu.au>
2024-10-30 10:44:28 +1100

    Fix bug on menuitem flow issue

--- src/components/common/dropdown/CommonSelect.tsx
+++ src/components/common/dropdown/CommonSelect.tsx
@@ -5,7 +5,7 @@ import {
   SelectChangeEvent,
   SxProps,
 } from "@mui/material";
-import { FC, useState } from "react";
+import { FC, useCallback, useEffect, useRef, useState } from "react";
 import { IconProps } from "../../icon/types";
 import { useDetailPageContext } from "../../../pages/detail-page/context/detail-page-context";
 
@@ -38,23 +38,68 @@ const CommonSelect: FC<CommonSelectProps> = ({
   onSelectCallback,
   sx,
 }) => {
+  const selectRef = useRef<HTMLDivElement>(null);
+  const [open, setOpen] = useState(false);
   const [selectedItem, setSelectedItem] = useState<string>(items[0].value);
   const { isCollectionNotFound } = useDetailPageContext();
 
-  const handleOnChange = (event: SelectChangeEvent<string>) => {
-    const selectedItem = event.target.value as string;
-    setSelectedItem(selectedItem);
-    if (onSelectCallback) onSelectCallback(selectedItem);
-  };
+  const handleOnChange = useCallback(
+    (event: SelectChangeEvent<string>) => {
+      const selectedItem = event.target.value as string;
+      setSelectedItem(selectedItem);
+      onSelectCallback && onSelectCallback(selectedItem);
+    },
+    [onSelectCallback]
+  );
+  // If the select is outside of viewport, close the menu item
+  // otherwise the menuitem will flow on top of screen.
+  const checkIfSelectIsOffScreen = useCallback(() => {
+    if (selectRef.current) {
+      const rect = selectRef.current.getBoundingClientRect();
+
+      // Check if any part of the Select component is outside the viewport
+      const isOffScreenNow =
+        rect.top < 0 ||
+        rect.left < 0 ||
+        rect.bottom >
+          (window.innerHeight || document.documentElement.clientHeight) ||
+        rect.right >
+          (window.innerWidth || document.documentElement.clientWidth);
+
+      isOffScreenNow && setOpen(false);
+    }
+  }, [selectRef]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkIfSelectIsOffScreen);
+    window.addEventListener("resize", checkIfSelectIsOffScreen); // Also handle viewport resizing
+    checkIfSelectIsOffScreen(); // Initial check
+
+    // Cleanup listeners on component unmount
+    return () => {
+      window.removeEventListener("scroll", checkIfSelectIsOffScreen);
+      window.removeEventListener("resize", checkIfSelectIsOffScreen);
+    };
+  }, [checkIfSelectIsOffScreen]);
 
   return (
     <FormControl fullWidth disabled={isCollectionNotFound}>
       <Select
+        ref={selectRef}
         value={selectedItem}
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
         onChange={handleOnChange}
         sx={{
           ...DEFAULT_SELECT_STYLE,
           ...sx,
+        }}
+        MenuProps={{
+          // Make the menuitem stick to select on scroll, however
+          // this cannot fix issue where the menuitem flow on top
+          // even select is off viewport
+          disableScrollLock: true,
         }}
       >
         {items.map((item) => (
