8794bce6d67bf61af3e5616a60591e8a177391e2
AR-RIFAT <ar.rifat.954@gmail.com>
2025-03-11 18:52:03 +1100

    test less buttons on mobile

--- playwright/tests/search_page/test_search_page.py
+++ playwright/tests/search_page/test_search_page.py
@@ -6,7 +6,11 @@ from pages.landing_page import LandingPage
 from pages.search_page import SearchPage
 
 
-def test_map_expand_toggle(desktop_page: Page) -> None:
+def test_map_full_screen_toggle(desktop_page: Page) -> None:
+    """
+    Verifies that the map can be toggled to full screen and back
+    to the default view.
+    """
     landing_page = LandingPage(desktop_page)
     search_page = SearchPage(desktop_page)
 
@@ -14,39 +18,35 @@ def test_map_expand_toggle(desktop_page: Page) -> None:
     landing_page.search.click_search_button()
 
     expect(search_page.result_list).to_be_visible()
-    search_page.map_toggle_control_button.click()
+    search_page.map.full_screen_toggle_button.click()
     expect(search_page.result_list).not_to_be_visible()
-    search_page.map_toggle_control_button.click()
+    search_page.map.full_screen_toggle_button.click()
     expect(search_page.result_list).to_be_visible()
     expect(search_page.result_card_list).not_to_have_count(0)
 
 
-@pytest.mark.parametrize(
-    'view_type',
-    [
-        'Grid and Map',
-    ],
-)
-def test_grid_and_map_view(desktop_page: Page, view_type: str) -> None:
+def test_grid_and_map_view(desktop_page: Page) -> None:
+    """
+    Verifies that selecting 'Grid and Map' from the 'View' options
+    displays the result list in grid view.
+    """
     landing_page = LandingPage(desktop_page)
     search_page = SearchPage(desktop_page)
 
     landing_page.load()
     landing_page.search.click_search_button()
 
-    search_page.result_layout_button.click()
-    search_page.click_text(view_type)
+    search_page.result_view_button.click()
+    search_page.grid_and_map_view_button.click()
     expect(search_page.result_grid).to_be_visible()
     expect(search_page.result_card_grid).not_to_have_count(0)
 
 
-@pytest.mark.parametrize(
-    'view_type',
-    [
-        'Full Map View',
-    ],
-)
-def test_full_map_view(responsive_page: Page, view_type: str) -> None:
+def test_full_map_view(responsive_page: Page) -> None:
+    """
+    Verifies that selecting 'Full Map View' from the 'View' options
+    displays the map in full screen by hiding the results list.
+    """
     landing_page = LandingPage(responsive_page)
     search_page = SearchPage(responsive_page)
 
@@ -54,18 +54,16 @@ def test_full_map_view(responsive_page: Page, view_type: str) -> None:
     landing_page.search.click_search_button()
 
     expect(search_page.result_list).to_be_visible()
-    search_page.result_layout_button.click()
-    search_page.click_text(view_type)
+    search_page.result_view_button.click()
+    search_page.full_map_view_button.click()
     expect(search_page.result_list).not_to_be_visible()
 
 
-@pytest.mark.parametrize(
-    'view_type',
-    [
-        'Full List View',
-    ],
-)
-def test_full_list_view(desktop_page: Page, view_type: str) -> None:
+def test_full_list_view(desktop_page: Page) -> None:
+    """
+    Verifies that selecting 'Full List View' from the 'View' options hides
+    the map and displays only the search result list on the entire screen.
+    """
     landing_page = LandingPage(desktop_page)
     search_page = SearchPage(desktop_page)
 
@@ -73,8 +71,8 @@ def test_full_list_view(desktop_page: Page, view_type: str) -> None:
     landing_page.search.click_search_button()
 
     expect(search_page.main_map).to_be_visible()
-    search_page.result_layout_button.click()
-    search_page.click_text(view_type)
+    search_page.result_view_button.click()
+    search_page.full_list_view_button.click()
     expect(search_page.main_map).not_to_be_visible()
     expect(search_page.result_list).to_be_visible()
     expect(search_page.bookmark_list).to_be_visible()
@@ -98,6 +96,10 @@ def test_show_more_results(
     chunk_2_first_data: str,
     chunk_2_last_data: str,
 ) -> None:
+    """
+    Verifies that the 'Show more results' button loads the next chunk of
+    search results and hides the button when all results have been loaded.
+    """
     landing_page = LandingPage(desktop_page)
     search_page = SearchPage(desktop_page)
 
@@ -137,6 +139,10 @@ def test_show_more_results(
 def test_links_button_navigates_to_detail_links_tab(
     desktop_page: Page, link_title: str
 ) -> None:
+    """
+    Verifies that clicking the 'Links' button on a result card
+    navigates to the detail page tab, where the 'Links' are listed.
+    """
     landing_page = LandingPage(desktop_page)
     search_page = SearchPage(desktop_page)
     detail_page = DetailPage(desktop_page)
@@ -150,3 +156,42 @@ def test_links_button_navigates_to_detail_links_tab(
     first_result.hover()
     first_result.get_by_label('Links').click()
     expect(detail_page.get_text(link_title)).to_be_visible()
+
+
+def test_view_options_on_mobile(mobile_page: Page) -> None:
+    """
+    Verifies that the 'View' combobox shows fewer options on mobile.
+    """
+    landing_page = LandingPage(mobile_page)
+    search_page = SearchPage(mobile_page)
+
+    landing_page.load()
+    landing_page.search.click_search_button()
+    search_page.wait_for_search_to_complete()
+
+    search_page.result_view_button.click()
+    expect(search_page.full_map_view_button).to_be_visible()
+    expect(search_page.full_list_view_button).to_be_visible()
+    expect(search_page.list_and_map_view_button).not_to_be_visible()
+    expect(search_page.grid_and_map_view_button).not_to_be_visible()
+
+
+def test_buttons_disappear_on_mobile(mobile_page: Page) -> None:
+    """
+    Verifies that some buttons disappear on mobile when
+    the 'Full Map View' option is selected.
+    """
+    landing_page = LandingPage(mobile_page)
+    search_page = SearchPage(mobile_page)
+
+    landing_page.load()
+    landing_page.search.click_search_button()
+    search_page.wait_for_search_to_complete()
+
+    search_page.result_view_button.click()
+    search_page.full_map_view_button.click()
+    expect(search_page.result_sort_button).not_to_be_visible()
+    expect(search_page.result_view_button).not_to_be_visible()
+    expect(search_page.bookmark_list).not_to_be_visible()
+    expect(search_page.map.zoom_in_button).not_to_be_visible()
+    expect(search_page.map.zoom_out_button).not_to_be_visible()
