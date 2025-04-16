5a0f24db80670fcb52370acad8aab92293380df1
AR-RIFAT <ar.rifat.954@gmail.com>
2025-04-16 13:35:58 +1000

    check BASE_URL

--- playwright/scripts/wait-for-server.sh
+++ playwright/scripts/wait-for-server.sh
@@ -2,9 +2,11 @@
 
 echo "Waiting for the web server to start"
 
-timeout=360
+timeout=150
 interval=1
 elapsed=0
+
+echo "BASE_URL is set to: $BASE_URL"
 
 while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $BASE_URL)" != "200" && $elapsed -lt $timeout ]]; do
   sleep $interval
