6289a1afced884b6fdbd8971969de8b324f3a67e
rng <manfai.ng@utas.edu.au>
2025-04-23 16:47:49 +1000

    Add popup

--- src/components/map/mapbox/layers/HexbinLayer.tsx
+++ src/components/map/mapbox/layers/HexbinLayer.tsx
@@ -1,44 +1,17 @@
-import React, { FC, useContext, useEffect, useRef } from "react";
+import { FC, useContext, useEffect, useRef } from "react";
 import { HexagonLayer } from "@deck.gl/aggregation-layers";
 import MapContext from "../MapContext";
 import { LayerBasicType } from "./Layers";
 import { Feature, Point } from "geojson";
-import { Box } from "@mui/material";
 import { MapboxOverlay } from "@deck.gl/mapbox";
+import { Popup } from "mapbox-gl";
+import { InnerHtmlBuilder } from "../../../../utils/HtmlUtils";
 
-// // Generate 100 random points around San Francisco
-// const generateRandomPoints = (
-//   centerLng: number,
-//   centerLat: number,
-//   count: number,
-//   radius: number = 0.05 // Approx 5km radius in degrees
-// ): FeatureCollection<Point> => {
-//   const features: Feature<Point>[] = [];
-//
-//   for (let i = 0; i < count; i++) {
-//     // Generate random offsets
-//     const angle = Math.random() * 2 * Math.PI;
-//     const distance = Math.sqrt(Math.random()) * radius * 10;
-//     const lng = centerLng + i;
-//     const lat = centerLat + i;
-//
-//     features.push({
-//       type: "Feature",
-//       geometry: {
-//         type: "Point",
-//         coordinates: [lng, lat],
-//       },
-//       properties: { id: i, count: i },
-//     });
-//   }
-//
-//   return {
-//     type: "FeatureCollection",
-//     features,
-//   };
-// };
+const MAPBOX_OVERLAY_HEXAGON_LAYER = "mapbox-overlay-hexagon-layer";
+
 const HexbinMap: FC<LayerBasicType> = ({ featureCollection }) => {
   const { map } = useContext(MapContext);
+  const popupRef = useRef<Popup | null>();
   const overlayRef = useRef<MapboxOverlay>();
 
   useEffect(() => {
@@ -48,7 +21,7 @@ const HexbinMap: FC<LayerBasicType> = ({ featureCollection }) => {
       if (featureCollection === undefined) return;
 
       const hexagonLayer = new HexagonLayer<Feature<Point>>({
-        id: "mapbox-overlay-hexagon-layer",
+        id: MAPBOX_OVERLAY_HEXAGON_LAYER,
         // data: "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf-bike-parking.json",
         //
         // gpuAggregation: true,
@@ -64,20 +37,80 @@ const HexbinMap: FC<LayerBasicType> = ({ featureCollection }) => {
           const coords = d.geometry.coordinates;
           return [coords[0], coords[1]]; // [lng, lat]
         },
-        getColorWeight: (point: Feature<Point>) => point?.properties?.count + 0,
-        getElevationWeight: (point: Feature<Point>) =>
-          point?.properties?.count + 0,
-        gpuAggregation: true,
-        extruded: true,
+        getColorWeight: (point: Feature<Point>) => point?.properties?.count,
+        // If you enable gpuAggregation the picking info will give less info
+        // due to the internal implementation. We do not see much diff in speed
+        // without gpuAggregation
+        gpuAggregation: false,
+        extruded: false,
+        pickable: true,
         radius: 50000,
+        opacity: 0.4,
+        colorRange: [
+          [255, 255, 178],
+          [254, 217, 118],
+          [254, 178, 76],
+          [253, 141, 60],
+          [240, 59, 32],
+          [189, 0, 38],
+        ],
         // getElevationValue: (v) => v.length,
-        // // elevationRange: [0, 1000],
-        // // getElevationValue: () => 500,
       });
 
       const overlay = new MapboxOverlay({
         interleaved: true,
         layers: [hexagonLayer],
+        onClick: (info) => {
+          if (info.picked && info.object) {
+            // Remove existing popup
+            if (popupRef.current) {
+              popupRef.current.remove();
+              popupRef.current = null;
+            }
+
+            // Create popup
+            popupRef.current = new Popup({
+              closeButton: true,
+              closeOnClick: false,
+              maxWidth: "none",
+              offset: [0, -5],
+            });
+
+            const points: Feature<Point>[] = info.object.points || [];
+
+            const smallestDate =
+              points.reduce((smallest, point) => {
+                const date = point.properties?.date;
+                if (typeof date !== "string") return smallest;
+                if (!smallest || date <= smallest) return date;
+                return smallest;
+              }, "") || "N/A";
+
+            const biggestDate =
+              points.reduce((smallest, point) => {
+                const date = point.properties?.date;
+                if (typeof date !== "string") return smallest;
+                if (!smallest || date > smallest) return date;
+                return smallest;
+              }, "") || "N/A";
+
+            const htmlBuilder = new InnerHtmlBuilder()
+              .addTitle("Data Records In This Area:")
+              .addText(
+                "Data Record Count: " +
+                  points.reduce((sum, point) => {
+                    const val = point.properties?.count ?? 0;
+                    return sum + (typeof val === "number" ? val : 0);
+                  }, 0)
+              )
+              .addRange("Time Range", smallestDate, biggestDate);
+
+            popupRef.current
+              .setLngLat(info.coordinate as [number, number])
+              .setHTML(htmlBuilder.getHtml())
+              .addTo(map);
+          }
+        },
       });
 
       overlayRef.current = overlay;
@@ -91,9 +124,7 @@ const HexbinMap: FC<LayerBasicType> = ({ featureCollection }) => {
     };
   }, [featureCollection, map]);
 
-  return (
-    <Box sx={{ height: "100%", display: "block" }} data-testId={"HexbinMap"} />
-  );
+  return null;
 };
 
 export default HexbinMap;
