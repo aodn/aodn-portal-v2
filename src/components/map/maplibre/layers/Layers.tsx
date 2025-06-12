c0728cf1f77d40a308a79130754d1649c5ec7901
rng <manfai.ng@utas.edu.au>
2025-06-12 16:38:41 +1000

    No longer use maplibre

--- src/components/map/maplibre/layers/Layers.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react";
-
-interface LayersProps {}
-
-const Layers = (props: PropsWithChildren<LayersProps>) => {
-  return <>{props.children}</>;
-};
-
-export default Layers;
