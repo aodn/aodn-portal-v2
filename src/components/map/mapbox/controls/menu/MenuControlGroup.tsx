b6d98d83c297b38e4446133531758b9b45537bdc
amber <yujie.bian@utas.edu.au>
2025-06-30 15:44:07 +1000

    update styles for menu control group

--- src/components/map/mapbox/controls/menu/MenuControlGroup.tsx
+++ src/components/map/mapbox/controls/menu/MenuControlGroup.tsx
@@ -13,30 +13,27 @@ const MenuControlGroup: FC<MenuControlGroupProps> = ({ children }) => {
   const childStyles: SxProps<Theme> = {
     // Target the mapboxgl-ctrl-group class on the child"s rendered div
     "&.mapboxgl-ctrl-group": {
-      borderRadius: "6px",
-      background: "red",
+      borderRadius: 0,
+      background: "#FFF",
       boxShadow: "4px 4px 4px 0px rgba(0, 0, 0, 0.10)",
       width: "45px",
-      margin: "10px",
+      marginX: "10px",
+      marginY: 0,
+      padding: "4px",
       display: "flex",
       flexDirection: "column",
       alignItems: "center",
-    },
-    // Target buttons or controls inside the mapboxgl-ctrl-group
-    "&.mapboxgl-ctrl-group .mapboxgl-ctrl": {
-      margin: "3.2px 0",
-      border: "none",
-      boxShadow: "none",
-      width: "100%",
-      display: "flex",
-      justifyContent: "center",
-      "& button": {
-        height: "40px",
-        borderRadius: "6px",
-        minWidth: "40px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+      // Modify this selector as needed for layout
+      "&:first-of-type": {
+        marginTop: "10px",
+        borderTopLeftRadius: "6px",
+        borderTopRightRadius: "6px",
+      },
+
+      "&:last-of-type": {
+        marginBottom: "10px",
+        borderBottomLeftRadius: "6px",
+        borderBottomRightRadius: "6px",
       },
     },
   };
