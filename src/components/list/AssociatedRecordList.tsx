41c36ce502b5af94227081aba2bc87459fe2eaa6
rng <manfai.ng@utas.edu.au>
2025-07-10 16:31:54 +1000

    fix bug on link

--- src/components/list/AssociatedRecordList.tsx
+++ src/components/list/AssociatedRecordList.tsx
@@ -1,5 +1,5 @@
 import { IAssociatedRecord } from "../common/store/OGCCollectionDefinitions";
-import React, { ReactNode, useMemo } from "react";
+import React, { memo, ReactNode, useMemo } from "react";
 import ExpandableList from "./ExpandableList";
 import { openInNewTab } from "../../utils/LinkUtils";
 import CollapseAssociatedRecordItem from "./listItem/CollapseAssociatedRecordItem";
@@ -12,36 +12,37 @@ interface AssociatedRecordListProps {
   records: IAssociatedRecord[];
 }
 
-const AssociatedRecordList: React.FC<AssociatedRecordListProps> = ({
-  title,
-  records,
-}) => {
-  const collapseComponents: ReactNode[] = useMemo(() => {
-    const components: ReactNode[] = [];
-    records?.map((record, index) => {
-      components.push(
-        <ItemBaseGrid key={index}>
-          <CollapseAssociatedRecordItem
-            titleAction={() =>
-              openInNewTab(`${pageDefault.details}/${record.uuid}`)
-            }
-            title={`${record.title}`}
-          >
-            <ExpandableTextArea
-              text={record.abstract}
-              isClickable
-              onClick={() =>
-                openInNewTab(`${pageDefault.details}/${record.uuid}`)
-              }
-            />
-          </CollapseAssociatedRecordItem>
-        </ItemBaseGrid>
-      );
-    });
-    return components;
-  }, [records]);
-
-  return <ExpandableList title={title} childrenList={collapseComponents} />;
+const openRecord = (uuid: string) => {
+  openInNewTab(`${pageDefault.details}/${uuid}`);
 };
 
+const AssociatedRecordList: React.FC<AssociatedRecordListProps> =
+  memo<AssociatedRecordListProps>(
+    ({ title, records }: AssociatedRecordListProps) => {
+      const collapseComponents: ReactNode[] = useMemo(() => {
+        const components: ReactNode[] = [];
+        records?.map((record, index) => {
+          components.push(
+            <ItemBaseGrid key={index}>
+              <CollapseAssociatedRecordItem
+                titleAction={() => openRecord(record.uuid)}
+                title={`${record.title}`}
+              >
+                <ExpandableTextArea
+                  text={record.abstract}
+                  isClickable
+                  onClick={() => openRecord(record.uuid)}
+                />
+              </CollapseAssociatedRecordItem>
+            </ItemBaseGrid>
+          );
+        });
+        return components;
+      }, [records]);
+
+      return <ExpandableList title={title} childrenList={collapseComponents} />;
+    }
+  );
+
+AssociatedRecordList.displayName = "AssociatedRecordList";
 export default AssociatedRecordList;
