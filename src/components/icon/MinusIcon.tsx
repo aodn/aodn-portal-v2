ec4c7867531a44ee65a0e9fcd69bac5e686bd354
amber <yujie.bian@utas.edu.au>
2025-07-30 13:41:21 +1000

    🐛 fix WCAG 2.2 compliance for show more button

--- src/components/icon/MinusIcon.tsx
+++ src/components/icon/MinusIcon.tsx
@@ -1,11 +1,12 @@
 import { SvgIcon } from "@mui/material";
+import rc8Theme from "../../styles/themeRC8";
 
 const MinusIcon = () => {
   return (
     <SvgIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
       <path
         d="M11.5 4.35156M18.25 11.1016H4.75"
-        stroke="#54BCEB"
+        stroke={rc8Theme.palette.primary1}
         strokeWidth="1.35"
         strokeLinecap="round"
         strokeLinejoin="round"
--- src/components/icon/PlusIcon.tsx
+++ src/components/icon/PlusIcon.tsx
@@ -1,11 +1,12 @@
 import { SvgIcon } from "@mui/material";
+import rc8Theme from "../../styles/themeRC8";
 
 const PlusIcon = () => {
   return (
     <SvgIcon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
       <path
         d="M11.5 4.35156V17.8516M18.25 11.1016H4.75"
-        stroke="#54BCEB"
+        stroke={rc8Theme.palette.primary1}
         strokeWidth="1.35"
         strokeLinecap="round"
         strokeLinejoin="round"
