78ef3ce31fb0d00523cdb0567156f4b8a848158b
amber <yujie.bian@utas.edu.au>
2025-09-04 16:38:55 +1000

    💄 implement NaArea component for details page sections not found

--- src/components/list/listItem/StyledNaItemGrid.tsx
+++ src/components/list/listItem/StyledNaItemGrid.tsx
@@ -1,13 +1,13 @@
 import { styled } from "@mui/material/styles";
 import { Grid } from "@mui/material";
+import rc8Theme from "../../../styles/themeRC8";
 
-const StyledNaItemGrid = styled(Grid)(({ theme }) => ({
-  backgroundColor: theme.palette.detail.na.light,
-  margin: theme.mp.sm,
-  borderRadius: theme.borderRadius.sm,
+const StyledNaItemGrid = styled(Grid)(() => ({
+  backgroundColor: rc8Theme.palette.primary6,
+  borderRadius: "4px",
   width: "95%",
-  padding: `${theme.mp.sm} ${theme.mp.xlg}`,
-  border: theme.border.detailNa,
+  height: "44px",
+  alignItems: "center",
 }));
 
 export default StyledNaItemGrid;
