9f157ea824d362d3ae223ef75dadfd19c84e6bbf
amber <yujie.bian@utas.edu.au>
2025-09-30 11:15:08 +1000

    fix conflicts

--- src/analytics/analyticsEvents.ts
+++ src/analytics/analyticsEvents.ts
@@ -5,8 +5,8 @@
  * - Use underscores (not hyphens) for GA4 compatibility
  */
 export enum AnalyticsEvent {
+  DOWNLOAD_CO_DATA = "download_co_data",
   COPY_CITATION_CLICK = "copy_citation_click",
   PAGE_RESPONSE_TIME = "page_response_time",
   SEARCH_RESULT_PARAMS = "search_result_params",
-  DOWNLOAD_CO_DATA = "download_co_data",
 }
