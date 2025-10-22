3270c2365bde2cd0448faedbfa0b72efa12bdac6
rng <manfai.ng@utas.edu.au>
2025-10-22 16:13:38 +1100

    Init checkin

---
+++ src/assets/icons/map/zoom_out.tsx
@@ -0,0 +1,53 @@
+export const ZoomOutIcon = () => (
+  <svg
+    width="39"
+    height="39"
+    viewBox="0 0 39 39"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g filter="url(#filter0_d_3464_37311)">
+      <circle cx="17.1953" cy="17.1953" r="15" fill="white" />
+    </g>
+    <path
+      d="M10.1953 18.1953V16.1953H24.1953V18.1953H10.1953Z"
+      fill="#595959"
+    />
+    <defs>
+      <filter
+        id="filter0_d_3464_37311"
+        x="-0.00178409"
+        y="-0.00178409"
+        width="38.7884"
+        height="38.7884"
+        filterUnits="userSpaceOnUse"
+      >
+        <feFlood floodOpacity="0" result="BackgroundImageFix" />
+        <feColorMatrix
+          in="SourceAlpha"
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+          result="hardAlpha"
+        />
+        <feOffset dx="2.1971" dy="2.1971" />
+        <feGaussianBlur stdDeviation="2.1971" />
+        <feComposite in2="hardAlpha" operator="out" />
+        <feColorMatrix
+          type="matrix"
+          values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.3 0"
+        />
+        <feBlend
+          mode="normal"
+          in2="BackgroundImageFix"
+          result="effect1_dropShadow_3464_37311"
+        />
+        <feBlend
+          mode="normal"
+          in="SourceGraphic"
+          in2="effect1_dropShadow_3464_37311"
+          result="shape"
+        />
+      </filter>
+    </defs>
+  </svg>
+);
--- src/components/map/mapbox/controls/NavigationControl.tsx
+++ src/components/map/mapbox/controls/NavigationControl.tsx
@@ -1,12 +1,82 @@
 import React, { useContext, useEffect, useState } from "react";
 import MapContext from "../MapContext";
-import { NavigationControl as MapboxNavigationControl } from "mapbox-gl";
+import {
+  NavigationControl as MapboxNavigationControl,
+  Map as Mapbox,
+} from "mapbox-gl";
+import { renderToStaticMarkup } from "react-dom/server";
+import { ZoomInIcon } from "../../../../assets/icons/map/zoom_in";
+import { ZoomOutIcon } from "../../../../assets/icons/map/zoom_out";
 
 interface NavigationControlProps {
   visible?: boolean;
   showCompass?: boolean;
   showZoom?: boolean;
   visualizePitch?: boolean;
+}
+
+const zoomInSvg = encodeURIComponent(renderToStaticMarkup(<ZoomInIcon />));
+const zoomOutSvg = encodeURIComponent(renderToStaticMarkup(<ZoomOutIcon />));
+
+class StyledNavigationControl extends MapboxNavigationControl {
+  constructor(options: {
+    showCompass?: boolean;
+    showZoom?: boolean;
+    visualizePitch?: boolean;
+  }) {
+    super(options); // Pass options to parent class
+  }
+
+  onAdd(map: Mapbox): HTMLElement {
+    const container = super.onAdd(map);
+    container.style.background = "none";
+    container.style.paddingBottom = "20px";
+    container.style.border = "none";
+
+    const zoomIn = container.querySelector(
+      ".mapboxgl-ctrl-zoom-in"
+    ) as HTMLButtonElement | null;
+
+    if (zoomIn) {
+      const icon = zoomIn.querySelector(
+        ".mapboxgl-ctrl-icon"
+      ) as HTMLSpanElement;
+
+      // Change the image to our design, hence keep function
+      if (icon) {
+        icon.style.backgroundImage = `url("data:image/svg+xml;charset=utf8,${zoomInSvg}")`;
+      }
+      zoomIn.style.minHeight = "35px";
+      zoomIn.style.minWidth = "35px";
+    }
+
+    const zoomOut = container.querySelector(
+      ".mapboxgl-ctrl-zoom-out"
+    ) as HTMLButtonElement | null;
+
+    if (zoomOut) {
+      const icon = zoomOut.querySelector(
+        ".mapboxgl-ctrl-icon"
+      ) as HTMLSpanElement;
+
+      // Change the image to our design, hence keep function
+      if (icon) {
+        icon.style.backgroundImage = `url("data:image/svg+xml;charset=utf8,${zoomOutSvg}")`;
+      }
+      zoomOut.style.minHeight = "35px";
+      zoomOut.style.minWidth = "35px";
+    }
+    // We do not need the compass
+    const compass = container.querySelector(
+      ".mapboxgl-ctrl-compass"
+    ) as HTMLButtonElement | null;
+
+    if (compass) {
+      compass.style.height = "0px";
+      compass.style.visibility = "none";
+    }
+    return container;
+  }
 }
 
 const NavigationControl = ({
@@ -16,15 +86,15 @@ const NavigationControl = ({
   visualizePitch = true,
 }: NavigationControlProps) => {
   const { map } = useContext(MapContext);
-  const [_, setControl] = useState<MapboxNavigationControl | undefined>(
+  const [_, setControl] = useState<StyledNavigationControl | undefined>(
     undefined
   );
 
   useEffect(() => {
     if (map !== null) {
-      setControl((prev: MapboxNavigationControl | undefined) => {
+      setControl((prev: StyledNavigationControl | undefined) => {
         if (!prev) {
-          const n = new MapboxNavigationControl({
+          const n = new StyledNavigationControl({
             showCompass: showCompass,
             showZoom: showZoom,
             visualizePitch: visualizePitch,
