dcaa8b95acc65e6749c8a896e68234df997d4a9f
rng <manfai.ng@utas.edu.au>
2025-12-29 16:09:35 +1100

    Handle exception case on file not found

--- playwright/mocks/api/wms_map.py
+++ playwright/mocks/api/wms_map.py
@@ -6,15 +6,19 @@ def handle_wms_map_tile_api(route: Route) -> None:
     route.fulfill(json={})  # Response not needed in the UI tests
 
 def handle_wms_map_layers_api(route: Route) -> None:
-    data_id = route.request.url.split('/')[-3]
-    json_data = load_json_data(f'wms_layers/{data_id}.json')
-    route.fulfill(json=json_data)  # Response not needed in the UI tests
+    try:
+        data_id = route.request.url.split('/')[-3]
+        json_data = load_json_data(f'wms_layers/{data_id}.json')
+        route.fulfill(json=json_data)  # Response not needed in the UI tests
+    except FileNotFoundError:
+        route.fulfill(json={})
 
 # Hardcode a response with type dateTime field so that the time slider button shown.
 # We may need to change it later to allow check on hidden time slider button
 def handle_wms_downloadable_fields_api(route: Route) -> None:
-    data_id = route.request.url.split('/')[-3]
-    json_data = load_json_data(f'wms_download_fields/{data_id}.json')
-    route.fulfill(
-        json=json_data
-    )
+    try:
+        data_id = route.request.url.split('/')[-3]
+        json_data = load_json_data(f'wms_download_fields/{data_id}.json')
+        route.fulfill(json=json_data)
+    except FileNotFoundError:
+        route.fulfill(json={})
