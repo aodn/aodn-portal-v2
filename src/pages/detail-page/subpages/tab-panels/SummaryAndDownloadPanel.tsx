d44f3db8b9bc9cacb2e4f1aec93f0b4a8622961f
Yuxuan HU <lydiahu_hn@hotmail.com>
2026-02-03 12:08:34 +1100

    revert map announce text

--- src/pages/detail-page/subpages/tab-panels/SummaryAndDownloadPanel.tsx
+++ src/pages/detail-page/subpages/tab-panels/SummaryAndDownloadPanel.tsx
@@ -438,7 +438,7 @@ const SummaryAndDownloadPanel: FC<SummaryAndDownloadPanelProps> = ({
                 panelId={mapContainerId}
                 projection={"mercator"} // Hexbin support this project or globe only
                 announcement={
-                  noMapPreview ? "Dataset preview is not available." : undefined
+                  noMapPreview ? "Map Preview Not Available" : undefined
                 }
                 onMoveEvent={handleMapChange}
                 onZoomEvent={handleMapChange}
