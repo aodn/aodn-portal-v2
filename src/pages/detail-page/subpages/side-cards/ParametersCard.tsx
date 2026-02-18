eb64c88d66b24940767abce61504a5080b3d36ae
amber <yujie.bian@utas.edu.au>
2026-02-18 13:29:39 +1100

    fix

--- src/pages/detail-page/subpages/side-cards/ParametersCard.tsx
+++ src/pages/detail-page/subpages/side-cards/ParametersCard.tsx
@@ -1,18 +1,33 @@
-import { FC, useMemo } from "react";
+import { FC, useCallback, useMemo } from "react";
 import { Chip, Stack } from "@mui/material";
 import { useDetailPageContext } from "../../context/detail-page-context";
 import SideCardContainer from "./SideCardContainer";
-import { formatToUrlParam } from "../../../../components/common/store/componentParamReducer";
-import { pageDefault } from "../../../../components/common/constants";
+import { useAppDispatch } from "../../../../components/common/store/hooks";
+import {
+  clearComponentParam,
+  updateParameterVocabs,
+} from "../../../../components/common/store/componentParamReducer";
+import useRedirectSearch from "../../../../hooks/useRedirectSearch";
+import { pageReferer } from "../../../../components/common/constants";
 import { portalTheme } from "../../../../styles";
-import { Link } from "react-router-dom";
 
 const ParametersCard: FC = () => {
   const { collection } = useDetailPageContext();
+  const dispatch = useAppDispatch();
+  const redirectSearch = useRedirectSearch();
 
   const parameterVocabs = useMemo(
     () => collection?.getParameterVocabs() ?? [],
     [collection]
+  );
+
+  const handleChipClick = useCallback(
+    (label: string) => {
+      dispatch(clearComponentParam());
+      dispatch(updateParameterVocabs([{ label }]));
+      redirectSearch(pageReferer.DETAIL_PAGE_REFERER);
+    },
+    [dispatch, redirectSearch]
   );
 
   if (parameterVocabs.length === 0) return null;
@@ -21,27 +36,23 @@ const ParametersCard: FC = () => {
     <SideCardContainer title="Parameters">
       <Stack direction="row" flexWrap="wrap" gap={1}>
         {parameterVocabs.map((label) => (
-          <Link
+          <Chip
             key={label}
-            to={`${pageDefault.search}?${formatToUrlParam({ parameterVocabs: [{ label }] })}`}
-            style={{ textDecoration: "none" }}
-          >
-            <Chip
-              label={label}
-              size="small"
-              clickable
-              sx={{
-                borderRadius: "6px",
-                height: "auto",
-                bgcolor: portalTheme.palette.primary6,
-                ...portalTheme.typography.body1Medium,
-                "& .MuiChip-label": {
-                  px: "12px",
-                  py: "6px",
-                },
-              }}
-            />
-          </Link>
+            label={label}
+            size="small"
+            clickable
+            onClick={() => handleChipClick(label)}
+            sx={{
+              borderRadius: "6px",
+              height: "auto",
+              bgcolor: portalTheme.palette.primary6,
+              ...portalTheme.typography.body1Medium,
+              "& .MuiChip-label": {
+                px: "12px",
+                py: "6px",
+              },
+            }}
+          />
         ))}
       </Stack>
     </SideCardContainer>
