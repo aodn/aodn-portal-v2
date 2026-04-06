9aa787d04807a6ccc14e765f8b1daa703935e982
rng <manfai.ng@utas.edu.au>
2026-04-06 14:46:47 +1000

    Fix unstable behavior

--- playwright/tests/search_page/map/test_map_state.py
+++ playwright/tests/search_page/map/test_map_state.py
@@ -83,16 +83,16 @@ def test_map_state_persists_across_page(desktop_page: Page) -> None:
 
     landing_page.load()
     landing_page.search.click_search_button()
-    search_page.map.wait_for_map_idle()
     search_page.wait_for_page_stabilization()
+    search_page.map.wait_for_map_idle()
 
     search_page.map.drag_map()
     search_page.wait_for_page_stabilization()
     search_page.map.zoom_in()
     search_page.wait_for_page_stabilization()
+    search_page.map.wait_for_map_idle()
 
     map_center = search_page.map.get_map_center()
-    search_page.map.wait_for_map_idle()
     map_zoom = search_page.map.get_map_zoom()
 
     search_page.first_result_title.click()
@@ -102,7 +102,6 @@ def test_map_state_persists_across_page(desktop_page: Page) -> None:
 
     new_map_center = search_page.map.get_map_center()
     new_map_zoom = search_page.map.get_map_zoom()
-    search_page.map.wait_for_map_idle()
 
     assert are_value_equal(map_zoom, new_map_zoom)
     assert are_coordinates_equal(map_center, new_map_center)
