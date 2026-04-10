4037a137614207e2cb3dc0f1fe34397e3145bf57
HuaizhiDai <huaizhi.dai@utas.edu.au>
2026-04-10 12:09:38 +1000

    cancel usage of degrade path

--- src/utils/HealthChecker.tsx
+++ src/utils/HealthChecker.tsx
@@ -1,43 +1,49 @@
-import { useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from "../components/common/store/hooks";
 import { fetchSystemHealthNoStore } from "../components/common/store/searchReducer";
-import { pageDefault } from "../components/common/constants";
+import DegradedPage from "../pages/error-page/DegradedPage";
 
-const HealthChecker = () => {
-  const navigate = useNavigate();
+interface HealthCheckerProps {
+  children?: React.ReactNode;
+}
+
+const HealthChecker: React.FC<HealthCheckerProps> = ({ children }) => {
   const dispatch = useAppDispatch();
-  const [_, setIsHealthy] = useState(true);
+  const [componentToRender, setComponentToRender] =
+    useState<React.ReactNode>(children);
+
+  const isHealthy = useCallback(async (): Promise<boolean> => {
+    try {
+      const health = await dispatch(fetchSystemHealthNoStore()).unwrap();
+      const status = (health.status || "UNKNOWN").toUpperCase();
+      const ogcStatus = (
+        health.components?.ogcApiHealth?.status || "UNKNOWN"
+      ).toUpperCase();
+      return status === "UP" && ogcStatus === "UP";
+    } catch (err) {
+      return false;
+    }
+  }, [dispatch]);
 
   useEffect(() => {
-    const checkHealth = () =>
-      dispatch(fetchSystemHealthNoStore())
-        .unwrap()
-        .then((health) => {
-          const status = health.status?.toUpperCase() || "UNKNOWN";
-
-          if (
-            status !== "UP" ||
-            health.components.ogcApiHealth.status !== "UP"
-          ) {
-            setIsHealthy(false);
-            navigate(pageDefault.degraded, { replace: true });
-          } else {
-            setIsHealthy(true);
-          }
-        })
-        .catch(() => {
-          setIsHealthy(false);
-          navigate(pageDefault.degraded, { replace: true });
-        });
+    const checkHealth = () => {
+      isHealthy().then((healthy) => {
+        if (!healthy) {
+          setComponentToRender(<DegradedPage />);
+        } else {
+          setComponentToRender(children); // render children if healthy
+        }
+      });
+    };
 
     checkHealth(); // initial check
     const interval = setInterval(checkHealth, 30000); // every 30s
 
     return () => clearInterval(interval);
-  }, [navigate, dispatch]);
+  }, [dispatch, isHealthy, children]);
 
-  return null; // invisible component
+  return componentToRender; // render children or degraded component
 };
 
 export default HealthChecker;
