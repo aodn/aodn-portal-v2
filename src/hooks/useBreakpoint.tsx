8f5fc334ab3e098d015d603aa37cdbb42420dc86
Lyn Long <linlin.long@utas.edu.au>
2026-04-28 15:17:33 +1000

    :lipstick:change breakpoint laptop to 1024px

--- src/hooks/useBreakpoint.tsx
+++ src/hooks/useBreakpoint.tsx
@@ -3,13 +3,13 @@ import { useMediaQuery, useTheme } from "@mui/material";
 const useBreakpoint = () => {
   const theme = useTheme();
   return {
-    isMobile: useMediaQuery(theme.breakpoints.down("sm")),
-    isTablet: useMediaQuery(theme.breakpoints.between("sm", "md")),
-    isLaptop: useMediaQuery(theme.breakpoints.between("md", "lg")),
-    isDesktop: useMediaQuery(theme.breakpoints.between("lg", "xl")),
-    is4K: useMediaQuery(theme.breakpoints.up("xl")),
-    isAboveDesktop: useMediaQuery(theme.breakpoints.up("lg")),
-    isUnderLaptop: useMediaQuery(theme.breakpoints.down("md")),
+    isMobile: useMediaQuery(theme.breakpoints.down("sm")), // < 768
+    isTablet: useMediaQuery(theme.breakpoints.between("sm", "md")), // 768–1024
+    isLaptop: useMediaQuery(theme.breakpoints.between("md", "lg")), // 1024–1440
+    isDesktop: useMediaQuery(theme.breakpoints.between("lg", "xl")), // 1440–1920
+    is4K: useMediaQuery(theme.breakpoints.up("xl")), // 1920+
+    isAboveDesktop: useMediaQuery(theme.breakpoints.up("lg")), // 1440+
+    isUnderLaptop: useMediaQuery(theme.breakpoints.down("md")), // < 1024
   };
 };
 
