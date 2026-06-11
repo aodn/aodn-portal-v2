1b50e5ae5866e32ca9105fd01d726b41b3055086
Yuxuan HU <lydiahu_hn@hotmail.com>
2026-06-11 11:44:21 +1000

    try to fix failed playwright

--- playwright/services/validators/search_url_filters/update_frequency_validator.py
+++ playwright/services/validators/search_url_filters/update_frequency_validator.py
@@ -8,5 +8,14 @@ class UpdateFrequencyValidator(BaseValidator):
     def validate(self) -> tuple[str, bool]:
         key = SearchFilterParams.UPDATE_FREQUENCY
         expected_value = self.config.update_frequency
-        is_valid = self.validate_param_value(key, expected_value)
-        return key, is_valid
+
+        if expected_value is None:
+            return key, not self.contains_expression(key)
+
+        has_expected = self.validate_param_value(key, expected_value)
+
+        if expected_value == 'other':
+            return key, has_expected
+
+        has_both = self.validate_param_value(key, 'both')
+        return key, has_expected and has_both
