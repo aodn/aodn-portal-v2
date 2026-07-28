ab03cb1f8a0b16cfe11625dccbffdb0878735178
Yuxuan HU <lydiahu_hn@hotmail.com>
2026-07-28 15:04:38 +1000

    update status mapping

--- src/components/result/ResultCardButtonGroup.tsx
+++ src/components/result/ResultCardButtonGroup.tsx
@@ -40,7 +40,7 @@ const renderStatusButton = (
   resultCardButtonConfig?: ResultCardButtonConfig
 ) => {
   const status = content?.getStatus()?.toLowerCase().trim();
-  if (status === Status.Completed) {
+  if (status?.startsWith(Status.Completed)) {
     return (
       <ResultCardButton
         startIcon={TaskAltSharpIcon}
@@ -50,7 +50,7 @@ const renderStatusButton = (
       />
     );
   }
-  if (status === Status.Ongoing) {
+  if (status?.startsWith(Status.Ongoing)) {
     return (
       <ResultCardButton
         startIcon={DoubleArrowIcon}
