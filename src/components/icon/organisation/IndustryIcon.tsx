51b29dd8541fac761d5ba0b265e2fc5548ccf3af
amber <yujie.bian@utas.edu.au>
2026-08-11 16:31:23 +1000

    chore: remove dead components and unused dependencies

--- src/components/icon/organisation/IndustryIcon.tsx
+++
@@ -1,8 +0,0 @@
-import { IndustrySVGIcon } from "../../../assets/filter-organisations/industry";
-import IconContainer from "../IconContainer";
-
-const IndustryIcon = () => {
-  return <IconContainer icon={IndustrySVGIcon} sx={{ border: "none" }} />;
-};
-
-export default IndustryIcon;
--- src/components/icon/platform/FixedStationIcon.tsx
+++
@@ -1,8 +0,0 @@
-import IconContainer from "../IconContainer";
-import { FixedStationSVGIcon } from "../../../assets/filter-platforms/fixed_station";
-
-const FixedStationIcon = () => {
-  return <IconContainer icon={FixedStationSVGIcon} sx={{ border: "none" }} />;
-};
-
-export default FixedStationIcon;
