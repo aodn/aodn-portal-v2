a86008c7f387c7408b34fd2c03760c169bf63df0
Lyn Long <linlin.long@utas.edu.au>
2026-08-12 15:36:54 +1000

    adjust popup height

--- src/components/map/mapbox/component/MapPopup.tsx
+++ src/components/map/mapbox/component/MapPopup.tsx
@@ -36,7 +36,7 @@ interface PopupConfig {
 
 const defaultPopupConfig: PopupConfig = {
   popupWidth: 280,
-  popupHeight: 410,
+  popupHeight: 400,
 };
 
 const renderLoadingBox = ({ popupHeight, popupWidth }: PopupConfig) => (
