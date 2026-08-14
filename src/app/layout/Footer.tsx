c9b71205d7367c7e1fffd108ed1d53adb9bc7e8c
rng <manfai.ng@utas.edu.au>
2026-08-14 10:02:27 +1000

    Do not show footer on search page

--- src/app/layout/Footer.tsx
+++ src/app/layout/Footer.tsx
@@ -35,6 +35,7 @@ import { pageDefault } from "@/components/common/constants";
 import { NorthIcon } from "@/components/icon/NorthIcon";
 import { BlueskyLogoIcon } from "@/components/icon/BlueskyLogoIcon";
 import { PAGE_CONTENT_WIDTH_LANDING } from "@/app/layout/constant";
+import React from "react";
 
 interface IconContainerProps {
   children: JSX.Element;
@@ -60,239 +61,251 @@ const version = import.meta.env.VITE_APP_VERSION;
 const Footer: FC = () => {
   const { pathname } = useLocation();
 
-  return (
-    <SectionContainer
-      contentAreaStyle={{
-        width: PAGE_CONTENT_WIDTH_LANDING,
-      }}
-    >
-      <Grid container paddingY={{ xs: padding.medium, sm: padding.quadruple }}>
-        <Grid
-          display="flex"
-          flexDirection={{ xs: "column-reverse", sm: "row" }}
-          justifyContent="space-between"
-          gap={{ xs: 2, sm: 0 }}
-          size={12}
+  switch (pathname) {
+    case pageDefault.search: {
+      return <React.Fragment />;
+    }
+    default:
+      return (
+        <SectionContainer
+          contentAreaStyle={{
+            width: PAGE_CONTENT_WIDTH_LANDING,
+          }}
         >
-          <AODNSiteLogo />
-          <Button onClick={() => scrollToTop()}>
-            <IconContainer>
-              <NorthIcon />
-            </IconContainer>
-            <Typography
-              paddingTop={0}
-              typography="title1Medium"
-              sx={{ lineHeight: "24px", ml: "8px" }}
-            >
-              Back to Top
-            </Typography>
-          </Button>
-        </Grid>
-        <Grid paddingY={padding.large} size={12}>
-          {!(pathname === pageDefault.search) && (
-            <Typography
-              color="#000"
-              fontSize={fontSize.subscription}
-              textAlign="left"
-              lineHeight={2}
-              px={1}
-            >
-              The IMOS Australian Ocean Data Network (AODN) stands at the
-              forefront of marine data management in Australia, providing an
-              essential infrastructure for the discovery, sharing and reuse of
-              comprehensive marine and climate data. By managing the IMOS data
-              collection program and incorporating contributions from
-              universities and government research agencies, AODN supports a
-              diverse range of users including researchers, data scientists,
-              government, and industry. Our commitment to making these data
-              freely available underscores our dedication to fostering an
-              informed and engaged public, promoting sustainable environmental
-              practices and driving economic growth through innovation.
-            </Typography>
-          )}
-        </Grid>
-
-        <Divider
-          sx={{
-            width: "100%",
-            borderColor: color.blue.dark,
-          }}
-        />
-        <Grid paddingY={padding.small} size={12}>
-          <Box
-            display="flex"
-            flexDirection={{ xs: "column", sm: "row" }}
-            justifyContent="space-between"
+          <Grid
+            container
+            paddingY={{ xs: padding.medium, sm: padding.quadruple }}
           >
-            <Tooltip title="Contact us by email" placement="top">
-              <Box
-                sx={{
-                  height: "100%",
-                  width: { xs: "100%", sm: "180px" },
-                  bgcolor: color.blue.extraDark,
-                  borderRadius: borderRadius.small,
-                }}
-              >
-                <Button
-                  sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    height: "100%",
-                    width: "100%",
-                  }}
-                  onClick={handleClickContactUs}
-                >
-                  <IconContainer sx={{ marginRight: margin.lg, color: "#fff" }}>
-                    <MailOutlineIcon />
-                  </IconContainer>
-                  <Typography
-                    color="#fff"
-                    paddingTop={0}
-                    fontSize={fontSize.info}
-                    letterSpacing={1}
-                    textAlign="center"
-                  >
-                    Contact Us
-                  </Typography>
-                </Button>
-              </Box>
-            </Tooltip>
-
-            <Box
-              display="flex"
-              flexDirection={{ xs: "column", sm: "row" }}
-              justifyContent="end"
-              gap={{ xs: 0, sm: 2 }}
-            >
-              <Button
-                onClick={() =>
-                  openInNewTab(`${pageDefault.url.IMOS}/terms-of-use`)
-                }
-              >
-                <Typography color="#000" paddingTop={gap.md}>
-                  Terms of Use
-                </Typography>
-              </Button>
-              <Button
-                onClick={() =>
-                  openInNewTab(
-                    `${pageDefault.url.IMOS}/resources/acknowledging-us`
-                  )
-                }
-              >
-                <Typography color="#000" paddingTop={gap.md}>
-                  Acknowledge Us
-                </Typography>
-              </Button>
-              <Button
-                onClick={() =>
-                  openInNewTab(`${pageDefault.url.IMOS}/conditions-of-use`)
-                }
-              >
-                <Typography color="#000" paddingTop={gap.md}>
-                  Conditions of Use
-                </Typography>
-              </Button>
-            </Box>
-          </Box>
-        </Grid>
-        <Divider
-          sx={{
-            width: "100%",
-            borderColor: color.blue.dark,
-          }}
-        />
-        <Grid paddingY={padding.large} size={12}>
-          <Grid container>
             <Grid
-              size={{
-                xs: 12,
-                sm: 7,
-              }}
+              display="flex"
+              flexDirection={{ xs: "column-reverse", sm: "row" }}
+              justifyContent="space-between"
+              gap={{ xs: 2, sm: 0 }}
+              size={12}
             >
-              <Stack
-                direction="row"
-                gap={2}
-                alignItems="center"
-                justifyContent="center"
-                marginBottom={2}
-              >
-                <Avatar src={traditional} />
-                <Typography color="#000" fontSize={fontSize.subscription}>
-                  IMOS acknowledges the Traditional Custodians and Elders of the
-                  land and sea on which we work and observe, and recognise them
-                  as Australia’s first marine scientists and carers of sea
-                  Country. We pay our respects to Aboriginal and Torres Strait
-                  Islander peoples past and present.
+              <AODNSiteLogo />
+              <Button onClick={() => scrollToTop()}>
+                <IconContainer>
+                  <NorthIcon />
+                </IconContainer>
+                <Typography
+                  paddingTop={0}
+                  typography="title1Medium"
+                  sx={{ lineHeight: "24px", ml: "8px" }}
+                >
+                  Back to Top
                 </Typography>
-              </Stack>
+              </Button>
+            </Grid>
+            <Grid paddingY={padding.large} size={12}>
               <Typography
                 color="#000"
                 fontSize={fontSize.subscription}
-                textAlign={{ xs: "center", sm: "left" }}
+                textAlign="left"
+                lineHeight={2}
+                px={1}
               >
-                Copyright © {currentYear}. All rights reserved. Version :&nbsp;
-                {version}
+                The IMOS Australian Ocean Data Network (AODN) stands at the
+                forefront of marine data management in Australia, providing an
+                essential infrastructure for the discovery, sharing and reuse of
+                comprehensive marine and climate data. By managing the IMOS data
+                collection program and incorporating contributions from
+                universities and government research agencies, AODN supports a
+                diverse range of users including researchers, data scientists,
+                government, and industry. Our commitment to making these data
+                freely available underscores our dedication to fostering an
+                informed and engaged public, promoting sustainable environmental
+                practices and driving economic growth through innovation.
               </Typography>
             </Grid>
-            <Grid
-              display="flex"
-              justifyContent={{ xs: "center", sm: "end" }}
-              alignItems="center"
-              size={{
-                xs: 12,
-                sm: 5,
+
+            <Divider
+              sx={{
+                width: "100%",
+                borderColor: color.blue.dark,
               }}
-            >
-              <Box>
-                <IconButton
-                  sx={{ color: color.blue.extraDark }}
-                  aria-label="Facebook"
-                  onClick={() =>
-                    openInNewTab(
-                      "https://www.facebook.com/IntegratedMarineObservingSystem"
-                    )
-                  }
+            />
+            <Grid paddingY={padding.small} size={12}>
+              <Box
+                display="flex"
+                flexDirection={{ xs: "column", sm: "row" }}
+                justifyContent="space-between"
+              >
+                <Tooltip title="Contact us by email" placement="top">
+                  <Box
+                    sx={{
+                      height: "100%",
+                      width: { xs: "100%", sm: "180px" },
+                      bgcolor: color.blue.extraDark,
+                      borderRadius: borderRadius.small,
+                    }}
+                  >
+                    <Button
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        height: "100%",
+                        width: "100%",
+                      }}
+                      onClick={handleClickContactUs}
+                    >
+                      <IconContainer
+                        sx={{ marginRight: margin.lg, color: "#fff" }}
+                      >
+                        <MailOutlineIcon />
+                      </IconContainer>
+                      <Typography
+                        color="#fff"
+                        paddingTop={0}
+                        fontSize={fontSize.info}
+                        letterSpacing={1}
+                        textAlign="center"
+                      >
+                        Contact Us
+                      </Typography>
+                    </Button>
+                  </Box>
+                </Tooltip>
+
+                <Box
+                  display="flex"
+                  flexDirection={{ xs: "column", sm: "row" }}
+                  justifyContent="end"
+                  gap={{ xs: 0, sm: 2 }}
                 >
-                  <FacebookIcon />
-                </IconButton>
-                <IconButton
-                  sx={{ color: color.blue.extraDark }}
-                  aria-label="LinkedIn"
-                  onClick={() =>
-                    openInNewTab("https://www.linkedin.com/company/18409795")
-                  }
-                >
-                  <LinkedInIcon />
-                </IconButton>
-                <IconButton
-                  sx={{ color: color.blue.extraDark }}
-                  aria-label="Bluesky"
-                  onClick={() =>
-                    openInNewTab(
-                      "https://bsky.app/profile/imos-aus.bsky.social"
-                    )
-                  }
-                >
-                  <BlueskyLogoIcon />
-                </IconButton>
-                <IconButton
-                  sx={{ color: color.blue.extraDark }}
-                  aria-label="Instagram"
-                  onClick={() =>
-                    openInNewTab("https://www.instagram.com/imos_australia")
-                  }
-                >
-                  <InstagramIcon />
-                </IconButton>
+                  <Button
+                    onClick={() =>
+                      openInNewTab(`${pageDefault.url.IMOS}/terms-of-use`)
+                    }
+                  >
+                    <Typography color="#000" paddingTop={gap.md}>
+                      Terms of Use
+                    </Typography>
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      openInNewTab(
+                        `${pageDefault.url.IMOS}/resources/acknowledging-us`
+                      )
+                    }
+                  >
+                    <Typography color="#000" paddingTop={gap.md}>
+                      Acknowledge Us
+                    </Typography>
+                  </Button>
+                  <Button
+                    onClick={() =>
+                      openInNewTab(`${pageDefault.url.IMOS}/conditions-of-use`)
+                    }
+                  >
+                    <Typography color="#000" paddingTop={gap.md}>
+                      Conditions of Use
+                    </Typography>
+                  </Button>
+                </Box>
               </Box>
             </Grid>
+            <Divider
+              sx={{
+                width: "100%",
+                borderColor: color.blue.dark,
+              }}
+            />
+            <Grid paddingY={padding.large} size={12}>
+              <Grid container>
+                <Grid
+                  size={{
+                    xs: 12,
+                    sm: 7,
+                  }}
+                >
+                  <Stack
+                    direction="row"
+                    gap={2}
+                    alignItems="center"
+                    justifyContent="center"
+                    marginBottom={2}
+                  >
+                    <Avatar src={traditional} />
+                    <Typography color="#000" fontSize={fontSize.subscription}>
+                      IMOS acknowledges the Traditional Custodians and Elders of
+                      the land and sea on which we work and observe, and
+                      recognise them as Australia’s first marine scientists and
+                      carers of sea Country. We pay our respects to Aboriginal
+                      and Torres Strait Islander peoples past and present.
+                    </Typography>
+                  </Stack>
+                  <Typography
+                    color="#000"
+                    fontSize={fontSize.subscription}
+                    textAlign={{ xs: "center", sm: "left" }}
+                  >
+                    Copyright © {currentYear}. All rights reserved. Version
+                    :&nbsp;
+                    {version}
+                  </Typography>
+                </Grid>
+                <Grid
+                  display="flex"
+                  justifyContent={{ xs: "center", sm: "end" }}
+                  alignItems="center"
+                  size={{
+                    xs: 12,
+                    sm: 5,
+                  }}
+                >
+                  <Box>
+                    <IconButton
+                      sx={{ color: color.blue.extraDark }}
+                      aria-label="Facebook"
+                      onClick={() =>
+                        openInNewTab(
+                          "https://www.facebook.com/IntegratedMarineObservingSystem"
+                        )
+                      }
+                    >
+                      <FacebookIcon />
+                    </IconButton>
+                    <IconButton
+                      sx={{ color: color.blue.extraDark }}
+                      aria-label="LinkedIn"
+                      onClick={() =>
+                        openInNewTab(
+                          "https://www.linkedin.com/company/18409795"
+                        )
+                      }
+                    >
+                      <LinkedInIcon />
+                    </IconButton>
+                    <IconButton
+                      sx={{ color: color.blue.extraDark }}
+                      aria-label="Bluesky"
+                      onClick={() =>
+                        openInNewTab(
+                          "https://bsky.app/profile/imos-aus.bsky.social"
+                        )
+                      }
+                    >
+                      <BlueskyLogoIcon />
+                    </IconButton>
+                    <IconButton
+                      sx={{ color: color.blue.extraDark }}
+                      aria-label="Instagram"
+                      onClick={() =>
+                        openInNewTab("https://www.instagram.com/imos_australia")
+                      }
+                    >
+                      <InstagramIcon />
+                    </IconButton>
+                  </Box>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
-      </Grid>
-    </SectionContainer>
-  );
+        </SectionContainer>
+      );
+  }
 };
 
 export default Footer;
