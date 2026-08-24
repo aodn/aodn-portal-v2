c74a60841e365e6f3cf8ab37164f2cc8fcbdc1f9
Weite Dai <weite.dai@utas.edu.au>
2026-08-24 15:07:30 +1000

    Ignore generated output dirs to prevent yarn lint OOM

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,4 +1,5 @@
 const js = require("@eslint/js");
+const { globalIgnores } = require("eslint/config");
 const tseslint = require("@typescript-eslint/eslint-plugin");
 const tsParser = require("@typescript-eslint/parser");
 const react = require("eslint-plugin-react");
@@ -9,7 +10,13 @@ const prettier = require("eslint-plugin-prettier");
 const prettierConfig = require("eslint-config-prettier");
 
 module.exports = [
-  { ignores: ["playwright/.venv/**", "playwright/pages/js_scripts/**"] },
+  globalIgnores([
+    "dist/**",
+    "coverage/**",
+    "html-reports/**",
+    "playwright/.venv/**",
+    "playwright/pages/js_scripts/**",
+  ]),
 
   js.configs.recommended,
 
