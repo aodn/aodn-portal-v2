f687d4dc0705f646eb9dd6142935d4348cbca9d1
rng <manfai.ng@utas.edu.au>
2026-09-01 21:42:39 +1000

    Fix one error and restate remove v7_startTransition

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -22,7 +22,6 @@ const app = () => {
               <RouterProvider
                 router={AppRouter}
                 fallbackElement={<Fallback />}
-                future={{ v7_startTransition: true }}
               />
             </AdminScreen>
           </ClipboardProvider>
